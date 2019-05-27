3e13ebeba575cd6653d81c2dbfb3db61d0d0d7a2
Prem Pratap Singh <46396311+prem-pratap@users.noreply.github.com>
2019-05-27 12:51:19 +0530

    This is the main code.

---
+++ recognizer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import cv2
+import numpy as np
+import os
+import Facerecognize as fr
+
+cap=cv2.VideoCapture(0)
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface.xml")
+font=cv2.FONT_ITALIC
+
+#calling our trainer
+face_data,face_id,dic=fr.training_dataset_and_labels()
+#features=np.load('trainingfaces.npy')
+#labels=np.load('traininglabel.npy')
+#calling LBPH face recognizer
+recognizer=cv2.face.LBPHFaceRecognizer_create()
+recognizer.train(face_data,np.array(face_id))
+#face_recognizer=fr.train_classifier(face_data,face_id)
+while cap.isOpened():
+    status,frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(frame,1.5,5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+        value,confidence=recognizer.predict(gray[y:y+h,x:x+w])
+        text=dic[str(value)]
+        #print(value)
+        if confidence < 40:
+            cv2.putText(frame,text,(x,y),font,2,(255,0,0),4)
+        #print(confidence)
+        #print(value)
+    cv2.imshow("live",frame)
+    if cv2.waitKey(30) & 0xff==ord('q'):
+        break
+cv2.destroyAllWindows()
+cap.release()
+
+
